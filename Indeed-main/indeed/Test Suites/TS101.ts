54d517ffda1ec2efa7848b21b90739dd18ca6df2
Thiranat <123710323+Thiranat@users.noreply.github.com>
2023-01-27 11:46:23 +0700

    Add files via upload

---
+++ Indeed-main/indeed/Test Suites/TS101.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS101</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>92e445e6-b124-4368-b0aa-434473761ceb</testSuiteGuid>
+   <testCaseLink>
+      <guid>1cef1229-7be9-458e-8dc2-55b65d92e208</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC103 Search Exception</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
